591c59ae4f35595bc6d2438276ae18b866cc9e09
macchiagabriele13 <macchia_gabriele@libero.it>
2023-01-16 16:14:22 +0100

    Commit- Creazione relazione one to many

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,16 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
+use App\Models\Type;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'description', 'slug', 'difficulty', 'languages'];
+    protected $fillable = ['title', 'description', 'slug', 'difficulty', 'languages', 'type_id'];
 
     public static function generateSlug($title)
     {
         $project_slug = Str::slug($title);
         return $project_slug;
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
